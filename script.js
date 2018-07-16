6b87330d131919d4f0bbb7955d9ad51ace12f034
Dragon331 <40502280+Dragon331@users.noreply.github.com>
2018-07-16 15:08:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function clickFunction1() {
+  document.getElementById("title").style.fontSize = "25px";
+}
+
+function clickFunction2() {
+  document.getElementById("title").style.fontSize = "16px";
+}
+
+function clickFunction3() {
+  alert("I'm a javascript function! I also took a coding class called Girls Who Code! That is where I learned how to make and personalize my website you are viewing!");
+}
